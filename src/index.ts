e3e428c90905b99228da9bab8aef441bf770daf9
raphaelcal <63277857+raphaelcal@users.noreply.github.com>
2021-01-18 16:34:16 -0600

    debug when not in prodcution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'graphql_first',
+        user: 'postgres',
+        password: 'postgres',
+        type: 'postgresql',
+        debug: !__prod__
+    });
+
+    const post = orm.create(Post, {title: "My first Post"});
+    await orm.em.persistAndFlush(post);
+}
+
 console.log("HELLO there WORLD")
